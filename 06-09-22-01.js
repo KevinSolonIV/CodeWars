79217fc1f632afc603bd76bba30d22029c37a381
Kevin Solon <kevinsoloniv@gmail.com>
2022-06-09 23:25:54 -0400

    Daily code wars submission

---
+++ 06-09-22-01.js
@@ -0,0 +1,7 @@
+function alphabetPosition(text) {
+    return text.toLowerCase()
+               .split('')
+               .map(x => 'abcdefghijklmnopqrstuvwxyz'.indexOf(x) + 1)
+               .filter(x => x > 0)
+               .join(' ');
+  }
