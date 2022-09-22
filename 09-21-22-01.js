8ac5cc0abb49deed38ac8d987a39f5c00284026e
Kevin J. Solon IV <kevinsoloniv@gmail.com>
2022-09-21 21:19:38 -0400

    Daily codewars submission

--- 09-21-22-01.js
+++ 09-21-22-01.js
@@ -1,8 +1,6 @@
-function deleteNth(arr,n){
-    let ans = []
-    // for each element of the input, if there are fewer than n copies of this element in ans, add it to ans
-    arr.forEach(elem => ans.filter(motif => motif === elem).length < n ?
-                ans.push(elem) :
-                console.log("too many of this motif: skipping " + elem))
-    return ans
+function getRealFloor(n) {
+    if(n === 0 || !Number.isInteger(n)) throw "Invalid input"
+    if(n < 0) return n
+    if(n < 13) return n - 1
+    else return n -2
   }
